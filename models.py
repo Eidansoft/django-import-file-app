ab2d0d722e1bfec04acd6bd63daa9d8d53bdda34
Alejandro Lorente <alejandro.lorente@qvantel.com>
2017-12-22 08:29:05 +0100

    simplifies the customizable export model

--- models.py
+++ models.py
@@ -4,11 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 from .forms import ImportForm
 
-# Create your models import here.
-class Example_Importer(models.Model):
+class ImporterBase(models.Model):
     class Meta:
         managed = False
-        verbose_name = 'my test'
+        verbose_name = 'Default importer'
 
     template_context = {
         'title_page': 'Import file app',
@@ -17,9 +16,16 @@ class Example_Importer(models.Model):
     }
 
     def process_file(self, file_path):
-        
         return "The file {} will be processed".format(file_path)
 
     @staticmethod
     def get_form(*args, **kwargs):
         return ImportForm(*args, **kwargs)
+
+# Create your models import here.
+class Example_Importer(ImporterBase):
+    class Meta:
+        managed = False
+        verbose_name = 'Default importer2'
+
+
