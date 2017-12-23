de897527dd9715abaa0add5b0803b6123b13c671
Alejandro Lorente <alejandro.lorente@qvantel.com>
2017-12-23 23:57:09 +0100

    get data from excel

--- models.py
+++ models.py
@@ -2,7 +2,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from datetime import datetime
 from .forms import ImportForm
+from importer.ProcesaExcel import ProcesaExcel, Celda
+from gastos.models import Spending
+
 
 class ImporterBase(models.Model):
     class Meta:
@@ -32,9 +36,32 @@ class ImporterBase(models.Model):
         return temp_file
 
 # Create your models import here.
-class Example_Importer(ImporterBase):
+class Xls_Importer(ImporterBase):
     class Meta:
         managed = False
-        verbose_name = 'Default importer'
+        verbose_name = 'Excel (xls)'
+
+    def process_file(self, request):
+        file_path = self.save_uploaded_file(request.FILES['file'])
+        excel_procesor = ProcesaExcel(
+            file_path,
+            Celda(5, 0),
+            Celda(5, 5)
+        )
+        data = excel_procesor.get_excel_data()
+
+        import ipdb; ipdb.set_trace(context=21)
+        for movement in data:
+            the_date = datetime.strptime(
+                movement.get(u'FECHA VALOR').value,
+                '%d/%m/%Y'
+            )
+            obj = Spending.objects.create(
+                concept = movement.get(u'DESCRIPCI\xd3N').value,
+                date = the_date,
+                amount = movement.get(u'IMPORTE (\u20ac)').value
+            )
+            pass
+
 
 
